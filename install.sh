765fc3a8eb66153c5877b2212df57091e6adc68b
Evan McIntire <mcintire.evan@gmail.com>
2017-06-09 17:57:08 -0400

    Fix install script
    * Remove changes to dhcpcd.conf because they're (maybe) not needed
    * Properly add quotes for the DAEMON_CONF in hostapd.conf

--- install.sh
+++ install.sh
@@ -22,11 +22,8 @@ cp "`dirname $0`/config/interfaces.client" /etc/network/
 cp "`dirname $0`/config/dnsmasq.conf" /etc/dnsmasq.conf
 
 # Edit hostapd file to point to the conf file
-sed -i 's/#DAEMON_CONF=""/DAEMON_CONF=\/etc\/hostapd\/hostapd\.conf/g' /etc/default/hostapd
+sed -i 's/#DAEMON_CONF=""/DAEMON_CONF=\"\/etc\/hostapd\/hostapd\.conf\"/g' /etc/default/hostapd
 
 # hostapd conf file
 # TODO: Config stuff (ssid, pass, wpa type)
 cp "`dirname $0`/config/hostapd.conf" /etc/hostapd/hostapd.conf
-
-# Edit dhcpcd so wlan0 gets managed by /etc/network/interfaces
-echo "denyinterfaces wlan0" >> /etc/dhcpcd.conf
